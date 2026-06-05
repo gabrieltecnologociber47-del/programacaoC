a0452939509a45ef71f523ff20968195ea3827f0
gabrieltecnologociber47-del <gabrieltecnologociber47@gmail.com>
2026-06-05 17:50:26 +0000

    Usei variantes como int, float, double e char. Usei especificadores de formato: %d, %f, %s e %c.

---
+++ Programa_Básico.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+ 
+    int idade = 26;
+    int quantidade = 1;
+    float altura = 1.70;
+    double peso = 90.3;
+    char letra = 'G';
+    char nome[20] = "Gabriel";
+
+    printf ("a idade é: %d\n", idade);
+    printf (" o nome é: %s\n", nome);
+    printf (" a altura é: %.2f\n", altura);
+    printf (" a letra é %c\n", letra);
+
+
+}
