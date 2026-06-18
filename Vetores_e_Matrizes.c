c7be28ff0dbb229ed5f219371eb1bbdad6f82ff2
gabrieltecnologociber47-del <gabrieltecnologociber47@gmail.com>
2026-06-18 16:45:25 +0000

    Vetores_e_Matrizes - Declaração, inicialização e acesso.

---
+++ Vetores_e_Matrizes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int numeros[5]; // Declaração de um vetor de inteiros
+    // Inicializando os elementos do vetor
+    numeros[0] = 10;
+    numeros[1] = 20;
+    numeros[2] = 30;
+    numeros[3] = 40;
+    numeros[4] = 50;
+    // Acessando elementos do vetor
+    printf("O primeiro elemento e: %d\n", numeros[0]);
+    printf("O segundo elemento e: %d\n", numeros[1]);
+    printf("O terceiro elemento e: %d\n", numeros[2]);
+    printf("O quarto elemento e: %d\n", numeros[3]);
+    printf("O quinto elemento e: %d\n", numeros[4]);
+
+    return 0;
+}
